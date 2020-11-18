ce63f798282ed4a0a98fb7cf20f00cf6e36a281f
keno80 <1211582428@qq.com>
2020-11-18 14:30:01 +0800

    添加其他图片

--- app/controller/items.js
+++ app/controller/items.js
@@ -3,6 +3,12 @@ const Controller = require('egg').Controller
 class itemController extends Controller {
   async item() {
     const info = await this.ctx.service.items.find(1)
+
+    this.ctx.body = {
+      code: 200,
+      item: info,
+      message: 'test'
+    }
   }
 }
 
--- app/service/items.js
+++ app/service/items.js
@@ -1,10 +1,13 @@
 const Service = require('egg').Service
+const path = require('path')
+const fs = require('fs')
 
 class itemService extends Service {
-  async find(id) {
-    const item = await this.app.mysql.get('items', {id})
-    console.log(item);
-  }
+	async find(id) {
+		// const item = await this.app.mysql.get('items', {id})
+		// console.log(item);
+	}
 }
+
 
 module.exports = itemService
